402db09cb6eafb80b7d99e21e26fa9c964825dae
cheadev <cheadev@naver.com>
2022-03-05 21:05:53 +0900

    Initial commit

---
+++ ex1/src/main/java/org/zerock/ex1/controller/SampleController.java
@@ -0,0 +1,12 @@
+package org.zerock.ex1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+    @GetMapping("/hello")
+    public String[] hello() {
+        return new String[]{"hello", "world"};
+    }
+}
